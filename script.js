b016cc749a61367a8e61706c102c1393d8e24f6c
Ingrid_Ramos <ingridrcs@gmail.com>
2022-01-26 00:29:48 -0300

    requisito 5

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const addLi = document.querySelector('.cart__items');
+
 function createProductImageElement(imageSource) {
   const img = document.createElement('img');
   img.className = 'item__image';
@@ -28,10 +29,31 @@ function createProductItemElement({ sku, name, image }) {
 function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 } 
+// Source: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/substring
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/parseFloat
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+// https://github.com/tryber/sd-018-b-project-shopping-cart/pull/53/files
 
+const newSession = document.createElement('section');
+  newSession.className = 'total-price';
+  newSession.innerHTML = 0;
+  const getCart = document.querySelector('.cart');
+  
+  function finalPrice() {
+  let plus = 0;
+  getCart.appendChild(newSession);
+  addLi.childNodes.forEach((item) => {
+    const search = item.innerText.indexOf('$');
+    const findPrice = item.innerText.substring(search + 1);
+    const lastPrice = parseFloat(findPrice);
+    plus += lastPrice;
+  });
+  newSession.innerText = plus;
+}
 function cartItemClickListener(event) {
   event.target.remove();
   saveCartItems(addLi.innerHTML);
+  finalPrice();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -50,12 +72,14 @@ async function addProduct() {
     const { id: sku, title: name, price: salePrice } = await fetchItem(itemId);
       addLi.appendChild(createCartItemElement({ sku, name, salePrice }));
       saveCartItems(addLi.innerHTML);
+      finalPrice();
     });
   });
 }
 function emptyLocalStorage() {
   addLi.innerHTML = '';
   saveCartItems(addLi);
+  finalPrice();
 }
 function eraseAll() {
 const allLi = document.querySelector('.empty-cart');
@@ -72,6 +96,7 @@ function saveOnPage() {
   const saveCard = getSavedCartItems();
   addLi.innerHTML = saveCard;
   addLi.childNodes.forEach((item) => item.addEventListener('click', cartItemClickListener));
+  finalPrice();
 }
 function loading() {
   const load = document.querySelector('.items');
@@ -84,6 +109,7 @@ function removeLoading() {
   const removeLoad = document.querySelector('.loading');
   return removeLoad.remove();
 }
+
 async function init() {
   loading();
   const data = await fetchProducts();
